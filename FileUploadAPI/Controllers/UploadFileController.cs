4872da814659305c279135df1472e2c0674947ee
freeman6 <freeman6@gmail.com>
2021-11-11 20:38:04 +0800

    完成使用官網提供的範例上傳檔案的API。

--- FileUploadAPI/Controllers/UploadFileController.cs
+++ FileUploadAPI/Controllers/UploadFileController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 
@@ -39,5 +40,59 @@ namespace FileUploadAPI.Controllers
 
             return result;
         }
+
+        /// <summary>
+        /// 以官網範例使用 MultipartFormDataStreamProvider 來完成上傳任務
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="HttpResponseException"></exception>
+        [HttpPost]
+        public async Task<HttpResponseMessage> PostFormData()
+        {
+            // Check if the request contains multipart/form-data.
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+            //取得目錄指定的地方
+            string root = System.Web.HttpContext.Current.Server.MapPath("~/App_Data");
+            var provider = new MultipartFormDataStreamProvider(root);
+
+            try
+            {
+                // Read the form data.塞入檔案,上傳文件,但是沒有副檔名，需要更改檔名
+                await Request.Content.ReadAsMultipartAsync(provider);
+
+                int i = 0;
+                // This illustrates how to get the file names.
+                foreach (MultipartFileData file in provider.FileData)
+                {
+                    //取得含有雙引號的
+                    string filename = file.Headers.ContentDisposition.FileName.Trim('"');
+                    //取得副檔名
+                    string fileExt = filename.Substring(filename.LastIndexOf('.'));
+
+                    System.IO.FileInfo fileInfo = new System.IO.FileInfo(file.LocalFileName);
+                    //fileinfo.Name 上傳後的文件路徑不含副檔名
+                    //修改文件名加上副檔名
+
+                    string newFileName = "test" + i + fileExt;
+                    i++;
+                    //最後保存文件路徑
+                    string saveUrl = System.IO.Path.Combine(root, newFileName);
+                    fileInfo.MoveTo(saveUrl);
+
+
+                    //System.Diagnostics.Trace.WriteLine(file.Headers.ContentDisposition.FileName);
+                    //System.Diagnostics.Trace.WriteLine("Server file path: " + file.LocalFileName);
+                }
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (System.Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+
     }
 }
